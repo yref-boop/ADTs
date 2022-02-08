856a29e794c265025b722fe3d8f46013b3a3a9ae
yref-boop <regofdez.yago@gmail.com>
2022-02-08 20:11:44 +0100

    pointer implementation

---
+++ rational_pointer.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "rational_pointer.h"
+
+//operation to create a rational number
+Rational createRational(int n, int d){
+    Rational temp;
+
+    temp = malloc(sizeof(*temp));
+    temp -> num = n;
+    temp -> den = d;
+    return temp;
+}
+
+//operation that returns the denominator of a rational number
+int denominator(Rational r){
+    return r -> den;
+}
+
+//operation that returns the numerator of a rational number
+int numerator(Rational r){
+    return r -> num;
+}
+
+//operation that returns the sum of two rational
+Rational sum(Rational r1, Rational r2){
+    Rational s;
+
+    s = malloc(sizeof(*s));
+    s -> num = r1 -> num * r2 -> den + r2 -> num * r1 -> den;
+    s -> den = r1 -> den * r2 -> den;
+    return s;
+}
