a32970960d40687db00aff50b477b0fedb084c12
yref-boop <regofdez.yago@gmail.com>
2022-04-02 22:24:05 +0200

    changed types file

--- p2/code/types.h
+++ p2/code/types.h
@@ -1,19 +1,22 @@
+/*
+ * TITLE: PROGRAMMING II LABS
+ * SUBTITLE: Practical 1
+ * AUTHOR 1: Alejandro Fernandez Vazquez LOGIN 1: a.fernandez9
+ * AUTHOR 2: Yago Fernandez Rego LOGIN 2: yago.fernandez.rego        
+ * GROUP: *6.1*
+ * DATE: 02 / 04 / 22
+ */
+
 #ifndef PRO2_2022_P2_TYPES_H
 #define PRO2_2022_P2_TYPES_H
 
 
 #define NAME_LENGTH_LIMIT 10
 
-
 typedef char tUserId[NAME_LENGTH_LIMIT];
-
 typedef char tProductId[NAME_LENGTH_LIMIT];
-
 typedef enum {book, painting} tProductCategory;
-
 typedef float tProductPrice;
-
 typedef int tBidCounter;
-
 
 #endif //PRO2_2022_P2_TYPES_H
