0b9471ce59d61901ede162ea0b1a904944970695
yref-boop <regofdez.yago@gmail.com>
2022-04-02 22:24:38 +0200

    implemented bid stack

--- p2/code/bid_stack.c
+++ p2/code/bid_stack.c
@@ -1,12 +1,62 @@
 /*
  * TITLE: PROGRAMMING II LABS
- * SUBTITLE: Practical 2
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * SUBTITLE: Practical 1
+ * AUTHOR 1: Alejandro Fernandez Vazquez LOGIN 1: a.fernandez9
+ * AUTHOR 2: Yago Fernandez Rego LOGIN 2: yago.fernandez.rego        
+ * GROUP: *6.1*
+ * DATE: 08 / 03 / 22
  */
 
 #include "bid_stack.h"
 
-/* Write your code here... */
+
+void createEmptyStack(tStack *stack){
+/*
+ *  Goal:   creates an empty stack
+ *  Input:  -stack (the stack)
+ *  Output: the wanted stack
+ *  Poscondition: the stack has no elements
+ */
+    stack -> top = -1;
+}
+
+
+bool push(tItemS item, tStack *stack){
+/*
+ *  Goal:   inserts an element at the top
+ *  Input:  -item (the item to be inserted)
+ *          -stack (the stack)
+ *  Output: a boolean literal
+ */
+    if (++stack -> top != MaxStackSize)
+        stack->array[stack->top] = item;
+}
+
+tStack pop (tStack *stack){
+/*
+ *  Goal:   removes the element on top
+ *  Input:  -stack (the stack)
+ *  Output: a the stack 
+ *  Precondition: the stack is not empty
+ */
+    stack -> top --;
+}
+
+tItemS peek (tStack stack){
+/*
+ *  Goal:   recover content of element at top
+ *  Input:  -stack (a stack)
+ *  Output: the element on top
+ *  Precondition: the stack is not empty
+ */
+    return stack.array[stack.top];
+}
+
+bool isEmptyStack (tStack stack){
+/*
+ *  Goal:   check if list is empty
+ *  Input:  -stack (a stack)
+ *  Output: a boolean literal
+ */
+    return (stack.top == -1);
+}
