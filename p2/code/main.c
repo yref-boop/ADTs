58830d6c9b73ca1d871d0decce7ae9480d1efd88
yref-boop <regofdez.yago@gmail.com>
2022-04-12 14:14:47 +0200

    added new function and auxiliars

--- p2/code/main.c
+++ p2/code/main.c
@@ -1,10 +1,10 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 2
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 1: Alejandro Fernandez Vazquez LOGIN 1: a.fernandez9
+ * AUTHOR 2: Yago Fernandez Rego LOGIN 2: yago.fernandez.rego
+ * GROUP: 6.1
+ * DATE: 12 / 04 / 22
  */
 
 #include <stdio.h>
@@ -14,13 +14,20 @@
 
 #define MAX_BUFFER 255
 
+#include "bid_stack.h"
+#include "product_list.h"
 
+void new (char *product, char *seller, char *category, char *price, tList *list);
 
-void processCommand(char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4) {
+void processCommand(char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4, tList *list) {
 
     switch (command) {
-        case 'N':
+        case 'N': {
+            printf("********************\n");
+            printf("%s %c: product %s seller %s category %s price %s\n", commandNumber, command, param1, param2, param3, param4);
+            new (param1, param2, param3, param4, list);
             break;
+        }
         case 'S':
             break;
         case 'B':
@@ -38,11 +45,61 @@ void processCommand(char *commandNumber, char command, char *param1, char *param
     }
 }
 
+char* extractCategory(tItemL prod) {
+
+    // Translate the category to text.
+    if (prod.productCategory == book)
+        return "book";
+    else
+        return "painting";
+}
+
+tItemL addProduct (char *product, char *seller, char *category, char *price){
+	// create the product
+	tItemL aux_product;
+
+	// get and store its properties:
+
+	// get the product id and seller
+    strcpy(aux_product.productId, product);
+    strcpy(aux_product.seller, seller);
+	// categorize the category
+    if (strcmp(category, "book") == 0)
+        aux_product.productCategory = book;
+    else
+        aux_product.productCategory = painting;
+	// set the price
+    float num_price = atof(price);
+    aux_product.productPrice = num_price;
+	// set bid counter to 0
+    aux_product.bidCounter = 0;
+	// create an empty bid stack
+	createEmptyStack(&aux_product.bidStack);
+    return aux_product;
+}
+
+void new (char *product, char *seller, char *category, char *price, tList *list){
+	// create a product with all the wanted properties
+	tItemL aux_product = addProduct(product, seller,category,price);
+
+	// check if an item with the same id already exists
+	if(findItem(aux_product.productId, *list) == LNULL) {
+		// if it doesn't, try to insert it on the list
+		if (insertItem(aux_product, list))
+			printf("* New: product %s seller %s category %s price %s\n", aux_product.productId, aux_product.seller, extractCategory(aux_product), price);
+		else printf("+ Error: New not possible\n");
+	}
+	else printf("+ Error: New not possible \n");
+}
+
 void readTasks(char *filename) {
     FILE *f = NULL;
     char *commandNumber, *command, *param1, *param2, *param3, *param4;
     const char delimiters[] = " \n\r";
     char buffer[MAX_BUFFER];
+
+	tList list;
+	createEmptyList(&list);
 
     f = fopen(filename, "r");
 
@@ -56,7 +113,7 @@ void readTasks(char *filename) {
             param3 = strtok(NULL, delimiters);
             param4 = strtok(NULL, delimiters);
 
-            processCommand(commandNumber, command[0], param1, param2, param3, param4);
+            processCommand(commandNumber, command[0], param1, param2, param3, param4, &list);
         }
 
         fclose(f);
