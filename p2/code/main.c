65798a03cc50d1f6aac6a2abbebd39c27b60c503
yref-boop <regofdez.yago@gmail.com>
2022-04-12 17:12:31 +0200

    stats finished and new checked

--- p2/code/main.c
+++ p2/code/main.c
@@ -18,6 +18,8 @@
 #include "product_list.h"
 
 void new (char *product, char *seller, char *category, char *price, tList *list);
+void stat (tList *list);
+
 
 void processCommand(char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4, tList *list) {
 
@@ -28,8 +30,12 @@ void processCommand(char *commandNumber, char command, char *param1, char *param
             new (param1, param2, param3, param4, list);
             break;
         }
-        case 'S':
+        case 'S': {
+            printf("********************\n");
+            printf("%s %c\n", commandNumber, command);
+            stat (list);
             break;
+        }
         case 'B':
             break;
         case 'D':
@@ -46,8 +52,7 @@ void processCommand(char *commandNumber, char command, char *param1, char *param
 }
 
 char* extractCategory(tItemL prod) {
-
-    // Translate the category to text.
+    // translate the category to text.
     if (prod.productCategory == book)
         return "book";
     else
@@ -90,6 +95,89 @@ void new (char *product, char *seller, char *category, char *price, tList *list)
 		else printf("+ Error: New not possible\n");
 	}
 	else printf("+ Error: New not possible \n");
+}
+
+void stat (tList *list){
+	// start auxiliar
+	tPosL pos;
+    tItemL aux_product;
+	tItemS aux_bid;
+	char top_bidder[NAME_LENGTH_LIMIT];
+
+	// table auxiliar data
+    int numBook = 0; 
+    int numPaint = 0;
+    float priceBook = 0; 
+    float pricePaint = 0;
+    float avgBook, avgPaint;
+
+	// top bid data
+	tItemL tbid_product;
+	tItemS tbid_bid;
+	float tbid_val = 0.0;
+
+    if (isEmptyList (*list) == true)
+        printf("+ Error: Stats not possible\n");
+    else {
+        pos = first(*list);
+        while (1) {
+			// set the auxiliar product as the one corresponding to the iteration
+            aux_product = getItem(pos, *list);
+			// if there are bids, check if bid value is bigger than last
+			if(!(isEmptyStack(aux_product.bidStack))){
+				tbid_bid = peek(aux_product.bidStack);
+				// if it is, store values 
+				if(tbid_bid.productPrice > tbid_val){
+					tbid_product = aux_product;
+					tbid_val = tbid_bid.productPrice;
+				}
+			}
+
+			// check if it is a book or a painint gand change corresponidng data
+            if(aux_product.productCategory == 0) {
+                numBook++;
+                priceBook += aux_product.productPrice;
+            }
+            else {
+                numPaint++;
+                pricePaint += aux_product.productPrice;
+            }
+			// print all the contents of the product
+			printf ("Product %s seller %s category %s price %.2f", aux_product.productId, aux_product.seller, extractCategory(aux_product), aux_product.productPrice);
+			if(aux_product.bidCounter == 0)
+				printf(". No bids\n");
+			else {
+				strcpy(top_bidder, peek(aux_product.bidStack).bidder);
+				printf("bids %d top bidder %s\n", aux_product.bidCounter, top_bidder);
+			}
+
+			// check condition to break the loop
+			if (pos == last(*list))
+                break;
+            else pos = next(pos, *list);
+        }
+		// calculate average values 
+        if (numBook == 0)
+            avgBook = 0;
+        else
+            avgBook = priceBook/numBook;
+
+        if (numPaint == 0)
+            avgPaint = 0;
+        else 
+            avgPaint = pricePaint/numPaint;
+		// print table 
+        printf("\nCategory  Products    Price  Average\n");
+        printf("Book      %8d %8.2f %8.2f\n", numBook, priceBook,avgBook);
+        printf("Painting  %8d %8.2f %8.2f\n", numPaint, pricePaint,avgPaint);
+
+		if (tbid_val == 0)
+			printf("Top bid not possible\n");
+		else{
+			aux_bid = peek(tbid_product.bidStack);
+			printf("Top bid: Product %s seller %s category %s price %.2f bidder %s top price %.2f increase %.2f%", tbid_product.productId, tbid_product.seller, extractCategory(tbid_product), tbid_product.productPrice, aux_bid.bidder, aux_bid.productPrice, (aux_bid.productPrice/tbid_product.productPrice)*100);
+		}
+    }
 }
 
 void readTasks(char *filename) {
@@ -140,9 +228,3 @@ int main(int nargs, char **args) {
 
     return 0;
 }
-
-
-
-
-
-
